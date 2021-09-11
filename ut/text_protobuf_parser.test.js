f7913c3b48714726130c0887e25ab8be215db707
h46incon <h46incon@gmail.com>
2021-09-11 18:21:35 +0800

    add some error test

--- ut/text_protobuf_parser.test.js
+++ ut/text_protobuf_parser.test.js
@@ -32,6 +32,10 @@ test('one field', () => {
     expect(parser.parse('_0123456789aAbBcBdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ:true')).toEqual(
         {_0123456789aAbBcBdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ : true}
     )
+    // invalid key name
+    expect(() => parser.parse('-')).toThrow()
+    expect(() => parser.parse(':')).toThrow()
+    expect(() => parser.parse(',')).toThrow()
 })
 
 test('true/false value', () => {
@@ -45,14 +49,22 @@ test('number value', () => {
     expect(parser.parse('k : 0')).toEqual({k : 0})
     expect(parser.parse('k : -1')).toEqual({k : -1})
     expect(parser.parse('k : 123')).toEqual({k : 123})
+    expect(parser.parse('k : 456 ')).toEqual({k : 456})
     expect(parser.parse('k : -1234567')).toEqual({k : -1234567})
     expect(parser.parse('k : 1.01')).toEqual({k : 1.01})
     expect(parser.parse('k : -1.01')).toEqual({k : -1.01})
 
+    // overflowed value save as string
     expect(parser.parse('k : 9007199254740991')).toEqual({k : 9007199254740991})
     expect(parser.parse('k : 9007199254740993')).toEqual({k : '9007199254740993'})
     expect(parser.parse('k : -9007199254740991')).toEqual({k : -9007199254740991})
     expect(parser.parse('k : -9007199254740993')).toEqual({k : '-9007199254740993'})
+
+    // invalid number format
+    expect(() => parser.parse('k : 1z')).toThrow()
+    expect(() => parser.parse('k : 1-')).toThrow()
+    expect(() => parser.parse('k : 1-2')).toThrow()
+    expect(() => parser.parse('k : 1.2.3')).toThrow()
 })
 
 test('string value', () => {
@@ -65,4 +77,10 @@ test('string value', () => {
     // simple escape
     expect(parser.parse(String.raw`k : " \\\/\" " `)).toEqual({k : String.raw` \/" `})
     expect(parser.parse(String.raw`k : " \b\f\n\r\t " `)).toEqual({k : " \b\f\n\r\t "})
+    // TODO: escape to bytes
+
+    // invalid string format
+    expect(() => parser.parse('k : "')).toThrow()
+    expect(() => parser.parse('k : "\\')).toThrow()
+    expect(() => parser.parse('k : "\\[')).toThrow()
 })
